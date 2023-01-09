425c3d296e49964dc4c60dbe2a80366d6826d422
knightlore <gnightshade@gmail.com>
2023-01-09 16:54:39 -0300

    force deploy

--- lib/translations.tsx
+++ lib/translations.tsx
@@ -1,21 +1,23 @@
-import { Custom404Strings } from '@ircsignpost/signpost-base/dist/src/404-page';
-import { ArticleContentStrings } from '@ircsignpost/signpost-base/dist/src/article-content';
-import { ArticlePageStrings } from '@ircsignpost/signpost-base/dist/src/article-page';
-import { CategoryStrings } from '@ircsignpost/signpost-base/dist/src/category-page';
-import { CookieBannerStrings } from '@ircsignpost/signpost-base/dist/src/cookie-banner';
-import { ErrorProps } from '@ircsignpost/signpost-base/dist/src/error';
-import { HeaderBannerStrings } from '@ircsignpost/signpost-base/dist/src/header-banner';
-import { HomePageStrings } from '@ircsignpost/signpost-base/dist/src/home-page';
-import { CardsListStrings } from '@ircsignpost/signpost-base/dist/src/home-page-cards-list';
-import { SearchBarStrings } from '@ircsignpost/signpost-base/dist/src/search-bar';
-import { SearchResultsPageStrings } from '@ircsignpost/signpost-base/dist/src/search-results-page';
-import { SearchResultsStrings } from '@ircsignpost/signpost-base/dist/src/search-results-page-content';
-import { SectionStrings } from '@ircsignpost/signpost-base/dist/src/section-page';
-import { ServiceMapStrings } from '@ircsignpost/signpost-base/dist/src/service-map';
-import { ShareButtonStrings } from '@ircsignpost/signpost-base/dist/src/share-button';
+import { Custom404Strings } from '@ircsignpost/signpost-base/dist/src/404-page'
+import { ArticleContentStrings } from '@ircsignpost/signpost-base/dist/src/article-content'
+import { ArticlePageStrings } from '@ircsignpost/signpost-base/dist/src/article-page'
+import { CategoryStrings } from '@ircsignpost/signpost-base/dist/src/category-page'
+import { CookieBannerStrings } from '@ircsignpost/signpost-base/dist/src/cookie-banner'
+import { ErrorProps } from '@ircsignpost/signpost-base/dist/src/error'
+import { HeaderBannerStrings } from '@ircsignpost/signpost-base/dist/src/header-banner'
+import { HomePageStrings } from '@ircsignpost/signpost-base/dist/src/home-page'
+import { CardsListStrings } from '@ircsignpost/signpost-base/dist/src/home-page-cards-list'
+import { SearchBarStrings } from '@ircsignpost/signpost-base/dist/src/search-bar'
+import { SearchResultsPageStrings } from '@ircsignpost/signpost-base/dist/src/search-results-page'
+import { SearchResultsStrings } from '@ircsignpost/signpost-base/dist/src/search-results-page-content'
+import { SectionStrings } from '@ircsignpost/signpost-base/dist/src/section-page'
+import { ServiceMapStrings } from '@ircsignpost/signpost-base/dist/src/service-map'
+import { ShareButtonStrings } from '@ircsignpost/signpost-base/dist/src/share-button'
 
-import { CustomMenuOverlayStrings } from './menu';
-import { SocialMediaLinks } from './social-media';
+import { CustomMenuOverlayStrings } from './menu'
+import { SocialMediaLinks } from './social-media'
+
+const UNUSED_CONST_ONLY_TO_TEST_A_PR = "ignore"
 
 /** General strings used on various pages. */
 export const COMMON_DYNAMIC_CONTENT_PLACEHOLDERS = [
@@ -35,7 +37,7 @@ export const COMMON_DYNAMIC_CONTENT_PLACEHOLDERS = [
   'default_last_updated',
   'default_article_reader_title',
   'default_banner_link_share_title',
-];
+]
 
 export const HOME_PAGE_DYNAMIC_CONTENT_PLACEHOLDERS = [
   // Header banner and social media strings.
@@ -69,18 +71,18 @@ export const HOME_PAGE_DYNAMIC_CONTENT_PLACEHOLDERS = [
   'DEFAULT_SERVICE_MAP_ALL_CITIES_OPTION',
   'DEFAULT_SERVICE_MAP_ALL_CATEGORIES_OPTION',
   'default_service_map_my_location_option',
-];
+]
 
-export const CATEGORY_PLACEHOLDERS = ['default_select_topic'];
+export const CATEGORY_PLACEHOLDERS = ['default_select_topic']
 
-export const SECTION_PLACEHOLDERS = ['default_select_topic'];
+export const SECTION_PLACEHOLDERS = ['default_select_topic']
 
 export const SEARCH_RESULTS_PLACEHOLDERS = [
   'default_search_results_found',
   'default_all_results_tab',
   'default_information_results_tab',
   'default_services_results_tab',
-];
+]
 
 export const ERROR_DYNAMIC_CONTENT_PLACEHOLDERS = [
   'default_error_indicator',
@@ -88,10 +90,10 @@ export const ERROR_DYNAMIC_CONTENT_PLACEHOLDERS = [
   'default_error_page_under_construction',
   'default_error_translation_missing',
   'default_error_home_button_title',
-];
+]
 
 export function populateSocialMediaLinks(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): SocialMediaLinks {
   return {
     facebookLink: {
@@ -110,22 +112,22 @@ export function populateSocialMediaLinks(dynamicContent: {
       title: dynamicContent['default_banner_instagram_title'],
       href: dynamicContent['CuentaNos_ElSalvador_instagram_link'],
     },
-  };
+  }
 }
 
 export function populateHeaderBannerStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): HeaderBannerStrings {
   return {
     welcomeTitle: dynamicContent['CuentaNos_ElSalvador_mission_statement'],
     socialMediaTitle: dynamicContent['default_banner_social_media_title'],
     socialMediaDescription:
       dynamicContent['default_banner_social_media_description'],
-  };
+  }
 }
 
 export function populateServiceMapStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): ServiceMapStrings {
   return {
     title: dynamicContent['default_service_map_title'],
@@ -146,68 +148,68 @@ export function populateServiceMapStrings(dynamicContent: {
     allCategoriesOption:
       dynamicContent['DEFAULT_SERVICE_MAP_ALL_CATEGORIES_OPTION'],
     myLocationOption: dynamicContent['default_service_map_my_location_option'],
-  };
+  }
 }
 
 /** Populate localized categories section strings from Dynamic content. */
 export function populateCategoriesSectionStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): CardsListStrings {
   return {
     title: dynamicContent['default_information_title'],
     description: dynamicContent['default_information_description'],
-  };
+  }
 }
 
 export function populateCookieBannerStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): CookieBannerStrings {
   return {
     content: dynamicContent['default_cookie_banner'],
     accept: dynamicContent['default_accept'],
     reject: dynamicContent['default_reject'],
-  };
+  }
 }
 
 export function getLastUpdatedLabel(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): string {
-  return dynamicContent['default_last_updated'];
+  return dynamicContent['default_last_updated']
 }
 
 export function getShareButtonStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): ShareButtonStrings {
   return {
     label: dynamicContent['default_share'],
     notificationText: dynamicContent['default_share_notification_text'],
     linkShareButton: dynamicContent['default_banner_link_share_title'],
-  };
+  }
 }
 
 export function generateArticleErrorProps(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): ErrorProps {
   return {
     title: dynamicContent['default_error_indicator'],
     subtitle: dynamicContent['default_error_page_under_construction'],
     description: dynamicContent['default_error_translation_missing'],
     homeButtonLabel: dynamicContent['default_error_home_button_title'],
-  };
+  }
 }
 
 export function generate404ErrorProps(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): ErrorProps {
   return {
     title: dynamicContent['default_error_indicator'],
     subtitle: dynamicContent['default_error_page_does_not_exist'],
     homeButtonLabel: dynamicContent['default_error_home_button_title'],
-  };
+  }
 }
 
 export function populateSearchResultsStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): SearchResultsStrings {
   return {
     lastEditedLabel: dynamicContent['default_last_updated'],
@@ -217,89 +219,89 @@ export function populateSearchResultsStrings(dynamicContent: {
       totalCount: number,
       query: string
     ) => {
-      return `${totalCount} ${dynamicContent['default_search_results_found']} "${query}"`;
+      return `${totalCount} ${dynamicContent['default_search_results_found']} "${query}"`
     },
-  };
+  }
 }
 
 export function getSelectTopicLabel(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): string {
-  return dynamicContent['default_select_topic'];
+  return dynamicContent['default_select_topic']
 }
 
 // TODO(annkats): add populateServiceMapStrings() once Service map becomes a Shared component.
 
 export function populateArticleContentStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): ArticleContentStrings {
   return {
     textReaderTitle: dynamicContent['default_article_reader_title'],
     shareButtonStrings: getShareButtonStrings(dynamicContent),
-  };
+  }
 }
 
 export function populateMenuOverlayStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): CustomMenuOverlayStrings {
   return {
     home: dynamicContent['default_menu_home_title'],
     information: dynamicContent['default_information_title'],
     about: dynamicContent['default_menu_about_title'],
-  };
+  }
 }
 
 export function populateHomePageStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): HomePageStrings {
   return {
     cardsListStrings: populateCategoriesSectionStrings(dynamicContent),
     cookieBannerStrings: populateCookieBannerStrings(dynamicContent),
     serviceMapStrings: populateServiceMapStrings(dynamicContent),
     searchBarStrings: populateSearchBarStrings(dynamicContent),
-  };
+  }
 }
 
 export function populateSearchBarStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): SearchBarStrings {
   return {
     searchHint: dynamicContent['default_search_hint'],
-  };
+  }
 }
 
 export function populateCategoryStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): CategoryStrings {
   return {
     cookieBannerStrings: populateCookieBannerStrings(dynamicContent),
     selectTopicLabel: getSelectTopicLabel(dynamicContent),
     searchBarStrings: populateSearchBarStrings(dynamicContent),
-  };
+  }
 }
 
 export function populateSectionStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): SectionStrings {
   return {
     cookieBannerStrings: populateCookieBannerStrings(dynamicContent),
     selectTopicLabel: getSelectTopicLabel(dynamicContent),
     searchBarStrings: populateSearchBarStrings(dynamicContent),
-  };
+  }
 }
 
 export function populateCustom404Strings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): Custom404Strings {
   return {
     errorStrings: generate404ErrorProps(dynamicContent),
     cookieBannerStrings: populateCookieBannerStrings(dynamicContent),
     searchBarStrings: populateSearchBarStrings(dynamicContent),
-  };
+  }
 }
 
 export function populateSearchResultsPageStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): SearchResultsPageStrings {
   return {
     searchBarStrings: populateSearchBarStrings(dynamicContent),
@@ -308,11 +310,11 @@ export function populateSearchResultsPageStrings(dynamicContent: {
     allResultsTabString: dynamicContent['default_all_results_tab'],
     informationTabString: dynamicContent['default_information_results_tab'],
     servicesTabString: dynamicContent['default_services_results_tab'],
-  };
+  }
 }
 
 export function populateArticlePageStrings(dynamicContent: {
-  [key: string]: string;
+  [key: string]: string
 }): ArticlePageStrings {
   return {
     articleContentStrings: populateArticleContentStrings(dynamicContent),
@@ -320,5 +322,5 @@ export function populateArticlePageStrings(dynamicContent: {
     cookieBannerStrings: populateCookieBannerStrings(dynamicContent),
     articleErrorStrings: generateArticleErrorProps(dynamicContent),
     lastUpdatedLabel: getLastUpdatedLabel(dynamicContent),
-  };
+  }
 }

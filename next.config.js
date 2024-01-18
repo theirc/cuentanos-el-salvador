e27b6c5df08ee4418c9f31c8fdb88bf177e06347
Bianca Chami <b.chami39@gmail.com>
2024-01-18 15:54:07 +0000

    feat: updated file

--- next.config.js
+++ next.config.js
@@ -29,6 +29,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'signpost-elsalvador.zendesk.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'directus-irc.azurewebsites.net',
+      },
     ],
   },
   publicRuntimeConfig: {
